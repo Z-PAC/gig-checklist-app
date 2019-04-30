7b7b056b055930825a2b9f6807c61134224a940c
Z-PAC <jpc_ace@hotmail.com>
2019-04-30 17:50:32 +0100

    Adds react-redux and navigation dependencies

---
+++ screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+//import styles from './HomeScreen.styles'
+
+class HomeScreen extends React.Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+              <Text>Home Screen placeholder</Text>
+            </View>
+        );
+    }
+}
+
+export default HomeScreen
